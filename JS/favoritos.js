b4552419d107a3f00b526a1ea263b6e9bd766cce
juan207145 <juanpablozambranorojas207@gmail.com>
2025-10-06 18:55:04 +0000

    arreglo de favoritos

--- JS/favoritos.js
+++ JS/favoritos.js
@@ -1,4 +1,9 @@
-function favoritos(){
-    alert("favoritos")
+function Favoritos(){
+    let favoritos = JSON.parse(localStorage.getItem("favoritos")) || [];
+    if(favoritos.length == 0){
+        document.getElementById("root").innerHTML = "no hay favoritos"
+    }else{
+        document.getElementById("root").innerHTML = generarLista(favoritos)
+    }
+    
 }
-
